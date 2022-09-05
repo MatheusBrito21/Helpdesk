f9dae89e7b8364ec85e4487cae173fccb3a96f3f
Matheus Brito <matheusb790@gmail.com>
2022-09-05 13:14:13 -0300

    SecurityConfig

---
+++ src/main/java/com/matt/helpdesk/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.matt.helpdesk.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.authorizeRequests().anyRequest().permitAll().and().httpBasic();
+	}
+}
